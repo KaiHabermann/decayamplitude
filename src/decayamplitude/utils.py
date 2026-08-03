fe5391ff018b1a7a1b59750a16c3b0b94aadfe5b
Kai Habermann <kai.habermann@gmx.net>
2026-08-04 00:28:34 +0200

    Small fix

--- src/decayamplitude/utils.py
+++ src/decayamplitude/utils.py
@@ -52,7 +52,7 @@ def _create_function(names: list[str], ls_couplings: dict[int, dict[str, dict[tu
     parameters = [inspect.Parameter(name, inspect.Parameter.POSITIONAL_OR_KEYWORD) for name in full_names]
     sig = inspect.Signature(parameters)
     func.__signature__ = sig
-    return func, full_names
+    return func, full_names.copy()
 
 def sanitize(name: str) -> str:
     """
